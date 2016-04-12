77ea3dac67a793ab3efe9180c0033524edaed6b1
feitracylai <feitracylai@gmail.com>
2016-04-12 19:34:53 -0400

    Update config.php

--- include/config.php
+++ include/config.php
@@ -6,7 +6,7 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '2439Lona');
 define('DB_HOST', 'gcmchat.c81l6jttxgta.us-east-1.rds.amazonaws.com:3306');
 define('DB_NAME', 'gcm_chat');
-define('DB_PORT', '3306')
+define('DB_PORT', '3306');
 
 define("GOOGLE_API_KEY", "AIzaSyD0fyZCPXgCpe0I_Oo0WC0-czr2dmYn11s");
 
