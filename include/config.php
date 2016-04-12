9ef43e522c85f80a22b904e29a0784fc76c4aabd
feitracylai <feitracylai@gmail.com>
2016-04-12 17:48:56 -0400

    no message

---
+++ include/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'gcm_chat');
+
+define("GOOGLE_API_KEY", "AIzaSyD0fyZCPXgCpe0I_Oo0WC0-czr2dmYn11s");
+
+// push notification flags
+define('PUSH_FLAG_CHATROOM', 1);
+define('PUSH_FLAG_USER', 2);
+
+?>
