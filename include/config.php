c7ef48b4fdebe7dfb3df97716bc8e48865d950b2
feitracylai <feitracylai@gmail.com>
2016-04-14 15:55:11 -0400

    Update config.php

--- include/config.php
+++ include/config.php
@@ -7,7 +7,7 @@ define('DB_PASSWORD', '2439Lona');
 define('DB_HOST', 'gcmchat.c81l6jttxgta.us-east-1.rds.amazonaws.com');
 define('DB_NAME', 'gcm_chat');
 
-define("GOOGLE_API_KEY", "AIzaSyD0fyZCPXgCpe0I_Oo0WC0-czr2dmYn11s");
+define("GOOGLE_API_KEY", "AIzaSyDdjFeemcBi8Zi6VFMFG7RjS9JTM-As2jE");
 
 // push notification flags
 define('PUSH_FLAG_CHATROOM', 1);
