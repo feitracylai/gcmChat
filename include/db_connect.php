829c1ada4c25e139f8b4434ccc18c17f5dd91b4b
feitracylai <feitracylai@gmail.com>
2016-04-14 16:19:12 -0400

    Update db_connect.php

--- include/db_connect.php
+++ include/db_connect.php
@@ -22,7 +22,7 @@ class DbConnect {
 
         // Connecting to mysql database
         //$this->conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME, 3306);
-        $this->conn = mysqli_connect($_SERVER['DB_HOST'], $_SERVER['DB_USERNAME'], $_SERVER['DB_PASSWORD'], $_SERVER['DB_NAME'], $_SERVER['DB_PORT']);
+        $this->conn = mysqli_connect($_SERVER['gcmchat.c81l6jttxgta.us-east-1.rds.amazonaws.com'], $_SERVER['root'], $_SERVER['2439Lona'], $_SERVER['gcm_chat'], $_SERVER['3306']);
 
         // Check for database connection error
         if (mysqli_connect_errno()) {
